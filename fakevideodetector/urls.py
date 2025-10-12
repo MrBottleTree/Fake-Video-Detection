e923a54143430b1dcb74df528ba6123d7765cbe9
Vishrut Ramraj <65703492+MrBottleTree@users.noreply.github.com>
2025-10-12 17:30:57 +0530

    made the URLS for the app, integrating with views.py

--- fakevideodetector/urls.py
+++ fakevideodetector/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('start/', views.start_graph, name='start_graph'),
+    path('callback/', views.node_callback, name='node_callback'),
+]
